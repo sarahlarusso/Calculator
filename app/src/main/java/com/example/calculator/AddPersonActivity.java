dfa1cf7384828f72fb8482c3743ac1dcc2c61003
sarahlarusso <sjlarusso@wpi.edu>
2019-03-29 14:22:26 -0400

    Basic UI Design 2 (main screen plus addition screen plus input)

--- app/src/main/java/com/example/calculator/AddPersonActivity.java
+++ app/src/main/java/com/example/calculator/AddPersonActivity.java
@@ -2,6 +2,9 @@ package com.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddPersonActivity extends AppCompatActivity {
 
@@ -10,4 +13,20 @@ public class AddPersonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_person);
     }
-}
+
+    public void addPerson (View v) {
+        EditText name = findViewById(R.id.txtName);
+        EditText relation = findViewById(R.id.txtRelation);
+
+        String nameStr = name.getText().toString();
+        String relationStr = relation.getText().toString();
+
+        String TAG = "Add Person Page";
+        Log.d(TAG, "User input: " + nameStr);
+        Log.d(TAG, "User input: " + relationStr);
+
+    }
+
+
+
+    }
