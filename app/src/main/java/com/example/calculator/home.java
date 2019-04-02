16b49e897f813d797c29f51708a8cec16f06723c
Nikhil Krishnan <nikhilkrishnan09@gmail.com>
2019-04-02 12:13:39 -0400

    Implemented camera feature for the welcome screen

--- app/src/main/java/com/example/calculator/home.java
+++ app/src/main/java/com/example/calculator/home.java
@@ -1,7 +1,12 @@
 package com.example.calculator;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageButton btnCamera = findViewById(R.id.cameraButton);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String imageData = data.getDataString();
+
+
+
     }
 }
