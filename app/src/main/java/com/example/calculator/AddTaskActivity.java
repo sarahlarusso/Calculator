29fc251a49dc08119605ed6ce8515408649e45df
sarahlarusso <sjlarusso@wpi.edu>
2019-05-08 09:25:54 -0400

    Arraylist

--- app/src/main/java/com/example/calculator/AddTaskActivity.java
+++ app/src/main/java/com/example/calculator/AddTaskActivity.java
@@ -12,7 +12,9 @@ import java.util.ArrayList;
 
 
 public class AddTaskActivity extends AppCompatActivity {
+
     private CheckBox checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6;
+    ArrayList<String> tasksArrayList;
 
 
     @Override
@@ -21,10 +23,13 @@ public class AddTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
 
         Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        tasksArrayList = bundle.getStringArrayList("Tasks Array List");
     }
 
     public void okaytasks(View v){
                 Intent intent = new Intent(v.getContext(), ViewTaskActivity.class);
+                intent.putExtra("Tasks Array List", tasksArrayList);
                 v.getContext().startActivity(intent);
     }
 
@@ -36,28 +41,25 @@ public class AddTaskActivity extends AppCompatActivity {
         switch(v.getId()) {
             case R.id.checkBox1:
                 if (checked) {
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "You have to do this task",
-                            Toast.LENGTH_LONG); 
-                    toast.show();
+                    tasksArrayList.add("Eat Breakfast");
                 }
             case R.id.checkBox2:
                 if (checked){
                     Toast toast = Toast.makeText(getApplicationContext(),
-                            "You have to do this task",
+                            "2 task",
                             Toast.LENGTH_LONG);
                 toast.show();}
 
             case R.id.checkBox3:
                 if (checked){
                     Toast toast = Toast.makeText(getApplicationContext(),
-                            "You have to do this task",
+                            "3 task",
                             Toast.LENGTH_LONG);
                     toast.show();}
             case R.id.checkBox4:
                 if (checked){
                     Toast toast = Toast.makeText(getApplicationContext(),
-                            "You have to do this task",
+                            "4 task",
                             Toast.LENGTH_LONG);
                     toast.show();}
 
